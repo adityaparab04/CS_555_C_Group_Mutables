5565e46f352ae4b874caafdd3dfeeaf964464bf4
rominaehsanii <rominaehsanii@gmail.com>
2022-02-27 22:58:40 -0500

    Sign in page implemented

--- Mutables/Mutables/VCs/SignInVC.swift
+++ Mutables/Mutables/VCs/SignInVC.swift
@@ -48,4 +48,34 @@ class SignInVC: UIViewController, UITextFieldDelegate {
 
     }
 
+    @IBAction func arrowBtnPressed(_ sender: UIButton) {
+        guard let email = emailTF.text, let password = passwordTF.text else {return}
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        let passwordRegEx = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+        
+        let passwordPred = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        if emailPred.evaluate(with: email) == true && passwordPred.evaluate(with: password) == true {
+            Auth.auth().signIn(withEmail: email, password: password) { result, err in
+                if err != nil {
+                    let alert = UIAlertController(title: "Error Sign-in", message: err?.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction.init(title: "Try Again", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    print("Successfull Sign-in")
+                    self.performSegue(withIdentifier: "goToHomeVC", sender: self)
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "Invalid Credentials", message: "You have entered invalid credentials, please verify and try again", preferredStyle: .alert)
+            alert.addAction(UIAlertAction.init(title: "Try again", style: .default, handler: { action in
+                self.emailTF.text = ""
+                self.passwordTF.text = ""
+                self.emailTF.becomeFirstResponder()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
