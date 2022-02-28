962a8c3b22adb3e959ec885881bbb59265d16c27
pragati1698 <kprag007@gmail.com>
2022-02-28 16:02:41 -0500

    Forgot password page implemented

--- Mutables/Mutables/VCs/ForgotPasswordVC.swift
+++ Mutables/Mutables/VCs/ForgotPasswordVC.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class ForgotPasswordVC: UIViewController {
+class ForgotPasswordVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        emailTF.becomeFirstResponder()
+        textFieldDesign(textfield: emailTF, placeholderText: "you@lorem.com")
     }
     
 
-    /*
-    // MARK: - Navigation
+    func textFieldDesign(textfield: UITextField, placeholderText: String){
+        textfield.layer.cornerRadius = 20.0
+        textfield.clipsToBounds = true
+        textfield.layer.borderWidth = 1.0
+        textfield.layer.borderColor = UIColor.black.cgColor
+        
+        
+        textfield.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray]
+        )
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    @IBAction func arrowBtnPressed(_ sender: Any) {
+        guard let email = emailTF.text else {return}
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        if emailPred.evaluate(with: email) == true{
+            Auth.auth().sendPasswordReset(withEmail: email) { err in
+                if err != nil {
+                    let alert = UIAlertController(title: "Invalid email", message: err?.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction.init(title: "Try again", style: .default, handler: { action in
+                        self.emailTF.text = ""
+                        self.emailTF.becomeFirstResponder()
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    let alert = UIAlertController(title: "Link sent successfully", message: "Password reset link successfully sent to \(email)", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction.init(title: "Great!", style: .default, handler: { action in
+                        self.dismiss(animated: true, completion: nil)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "Invalid email", message: "\(email) is invalid, please verify and try again.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction.init(title: "Try again", style: .default, handler: { action in
+                self.emailTF.text = ""
+                self.emailTF.becomeFirstResponder()
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        
+        
+    }
+    
 }
