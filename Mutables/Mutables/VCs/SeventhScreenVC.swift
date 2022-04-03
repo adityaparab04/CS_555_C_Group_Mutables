1d054526fc41f970dcc11b9c3ed348c2869e6b4d
rahulpawar166 <rahulpawar166@gmail.com>
2022-04-03 19:46:20 -0400

    Seventh and Eighth story added, implemented, and music added

--- Mutables/Mutables/VCs/SeventhScreenVC.swift
+++ Mutables/Mutables/VCs/SeventhScreenVC.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class SeventhScreenVC: UIViewController {
+    
+    let music = Music()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        music.startMusic(fileName: "background", format: "mp3", volume: 0.3, loop: -1)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+        self.navigationItem.setHidesBackButton(true, animated: true)
     }
-    */
+    override func viewDidDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = false
+    }
 
 }
